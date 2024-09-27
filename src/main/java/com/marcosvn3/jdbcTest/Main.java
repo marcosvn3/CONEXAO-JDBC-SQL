9c357f8d8d176554623e7d4cf699702bbcf44cc6
Marcos santos <marcosvn3@academico.ufs.br>
2024-09-26 23:09:10 -0300

    Configura o Flyway para gerenciar as migrações do banco de dados

---
+++ src/main/java/com/marcosvn3/jdbcTest/Main.java
@@ -0,0 +1,22 @@
+package com.marcosvn3.jdbcTest;
+
+import com.marcosvn3.jdbcTest.persistence.EmployeeDAO;
+import org.flywaydb.core.Flyway;
+
+// Esta é a classe principal do programa
+public class Main {
+
+    // Cria uma instância estática de EmployeeDAO para ser usada em toda a aplicação
+    private final static EmployeeDAO employeeDAO = new EmployeeDAO();
+
+    public static void main(String[] args) {
+        // Configura o Flyway para gerenciar as migrações do banco de dados
+        var flyway = Flyway
+                .configure()
+                // Define a conexão com o banco de dados MySQL
+                .dataSource("jdbc:mysql://localhost:3306/test-jdbc", "root", "144571")
+                .load();
+        // Executa as migrações pendentes no banco de dados
+        flyway.migrate();
+    }
+}
