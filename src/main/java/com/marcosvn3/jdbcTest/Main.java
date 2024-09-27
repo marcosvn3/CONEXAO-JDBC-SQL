2f4681a2d38f63b0b63feb06f97da73445f8b1db
Marcos santos <marcosvn3@academico.ufs.br>
2024-09-27 11:41:00 -0300

    Testando metodos DAOEmployee.

--- src/main/java/com/marcosvn3/jdbcTest/Main.java
+++ src/main/java/com/marcosvn3/jdbcTest/Main.java
@@ -1,7 +1,11 @@
 package com.marcosvn3.jdbcTest;
 
 import com.marcosvn3.jdbcTest.persistence.EmployeeDAO;
+import com.marcosvn3.jdbcTest.persistence.entity.EmployeeEntity;
 import org.flywaydb.core.Flyway;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
 
 // Esta é a classe principal do programa
 public class Main {
@@ -14,9 +18,25 @@ public class Main {
         var flyway = Flyway
                 .configure()
                 // Define a conexão com o banco de dados MySQL
-                .dataSource("jdbc:mysql://localhost:3306/test-jdbc", "root", "0000000")
+                .dataSource("jdbc:mysql://localhost:3306/test-jdbc", "root", "144571")
                 .load();
         // Executa as migrações pendentes no banco de dados
         flyway.migrate();
+
+
+        /*var employee = new EmployeeEntity();
+        employee.setNome("Anthony s");
+        employee.setSalario(new BigDecimal(4000));
+        employee.setAniversario(OffsetDateTime.now().minusYears(22).minusDays(25));
+        System.out.println(employee);
+        employeeDAO.insert(employee);
+        System.out.println(employee);*/
+
+        //employeeDAO.findAll().forEach(System.out::println);
+
+
+        System.out.println(employeeDAO.findById(3));
+
+
     }
 }
