9cbf21f03f178de4ab27f8d9fbde661f2642f52c
Marcos santos <marcosvn3@academico.ufs.br>
2024-09-27 11:41:09 -0300

    update info

--- src/main/java/com/marcosvn3/jdbcTest/persistence/ConnectorUtil.java
+++ src/main/java/com/marcosvn3/jdbcTest/persistence/ConnectorUtil.java
@@ -15,6 +15,6 @@ public class ConnectorUtil {
     // TODO:Este método estático retorna uma conexão com o banco de dados.
     // O metodo pode lançar uma SQLException em caso de falha na conexão.
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection("jdbc:mysql://localhost:3306/test-jdbc", "root", "000000");
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/test-jdbc", "root", "144571");
     }
 }
