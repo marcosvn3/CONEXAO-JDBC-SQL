1f963c4286664761fd2cd5792c74b6b872d1c6bf
Marcos santos <marcosvn3@academico.ufs.br>
2024-09-26 23:06:48 -0300

    Initial commit

---
+++ src/test/java/com/marcosvn3/jdbcTest/JdbcTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcosvn3.jdbcTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
